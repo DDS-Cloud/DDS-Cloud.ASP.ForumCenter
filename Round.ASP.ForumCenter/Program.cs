e1821311bcde5674024e9a524b989aa794a64f04
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2024-12-15 00:42:38 +0800

    Add files via upload

---
+++ Round.ASP.ForumCenter/Program.cs
@@ -0,0 +1,44 @@
+using Round.ASP.ForumCenter.Models;
+using Round.ASP.ForumCenter.Models.Config;
+using Round.ASP.ForumCenter.Models.Server;
+using Round.ASP.ForumCenter.Models.User;
+
+namespace Round.ASP.ForumCenter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigCore.LoadingConfig();
+            Task.Run(() =>
+            {
+                APIServerCore.StartServerAsync();
+            });
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
